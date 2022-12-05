f1319d02db05e73ddb3838121e3b1b8f9fbdc86b
Maik Heinemann <maik.heinemann@voltavision.de>
2022-12-05 13:46:04 +0100

    05.12.2022

---
+++ 2k22/05/05_2.js
@@ -0,0 +1,41 @@
+const { readFileSync } = require("fs");
+
+const input = readFileSync('./05_input.txt', { encoding: 'utf-8' })
+
+const lines = input.split('\r\n').filter(line => !!line);
+
+const stackConfig = lines.filter(line => !line.includes('move'));
+const stepConfig = lines.filter(line => line.includes('move'));
+
+const stackNumbersString = stackConfig.filter(line => !line.includes('['))[0];
+const stackNumbers = stackNumbersString.match(/\d+/gm);
+const stackNumberIndices = stackNumbers.map(stackNumber => stackNumbersString.indexOf(stackNumber));
+
+const stacksWithoutNumbers = stackConfig.filter(line => line.includes('['));
+
+const stacks = stackNumberIndices.map(index => {
+    return stacksWithoutNumbers
+        .map(stackLine => {
+            if (stackLine.length < index) {
+                return null;
+            }
+            if (stackLine.charAt(index) === ' ') {
+                return null;
+            }
+            return stackLine.charAt(index);
+        })
+        .filter(crate => !!crate)
+        .reverse();
+});
+
+const moveCrates = (fromStackIndex, toStackIndex, amount) => {
+    stacks[toStackIndex].push(...stacks[fromStackIndex].splice(-amount));
+}
+
+stepConfig.forEach(line => {
+    const [amount, fromStackNumber, toStackNumber] = line.match(/\d+/gm).map(x => Number.parseInt(x, 10));
+    moveCrates(fromStackNumber - 1, toStackNumber - 1, amount);
+});
+
+
+console.log(stacks.map(stack => stack[stack.length - 1]).join(''));
