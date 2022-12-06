7ab1a1d11597c7f62c701f1e773e4ad85543816e
Maik Heinemann <maik.heinemann@voltavision.de>
2022-12-06 14:56:25 +0100

    06.12.2022

---
+++ 2k22/06/06_2.js
@@ -0,0 +1,22 @@
+const { readFileSync } = require("fs");
+
+let input = readFileSync('./06_input.txt', { encoding: 'utf-8' });
+
+const lengthOfDelimiter = 14;
+
+let startOfPacket = lengthOfDelimiter;
+
+while (input.length > lengthOfDelimiter) {
+    const sequence = input.substring(0, lengthOfDelimiter);
+    const chars = sequence.split('');
+    const uniqueChars = Array.from(new Set(chars));
+
+    if (uniqueChars.length === lengthOfDelimiter) {
+        break;
+    }
+
+    input = input.substring(1);
+    startOfPacket++;
+}
+
+console.log(startOfPacket);
